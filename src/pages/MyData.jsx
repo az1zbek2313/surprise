943eef4b6b956ee57669a6a39cc6a11eb391bd41
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-09-27 22:11:42 +0500

    biroz o'zgarish kiritildi

--- src/pages/MyData.jsx
+++ src/pages/MyData.jsx
@@ -113,7 +113,7 @@ function MyData() {
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 disabled={formDisabled}
-                defaultValue={myData?.name}
+                defaultValue={myData?.name.split(" ")[1]}
                 ref={nameRef}
                 required
               />
@@ -121,51 +121,27 @@ function MyData() {
                 htmlFor="floating_full_name"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
-                Fullname
+                Firstname
               </label>
             </div>
             <div className="relative z-0 w-full mb-5 group">
               <input
-                type="date"
-                name="floating_company"
-                id="floating_company"
-                className="block py-2.5 px-0 w-full text-sm text-gray-600 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                disabled={formDisabled}
-                defaultValue={"2004-09-18"}
-                required
-              />
-            </div>
-          </div>
-
-          <div className="grid md:grid-cols-2 md:gap-6">
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="email"
-                name="floating_email"
-                id="floating_email"
+                type="text"
+                name="floating_full_name"
+                id="floating_full_name"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 disabled={formDisabled}
-                defaultValue={"xoldarovazizbek04@gmail.com"}
+                defaultValue={myData?.name.split(" ")[0]}
+                ref={nameRef}
                 required
               />
               <label
-                htmlFor="floating_email"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                htmlFor="floating_full_name"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
-                Email address
+                Lastname
               </label>
-            </div>
-            <div className="relative z-0 w-full mb-5 group">
-              <select
-                name="floating_sex"
-                id="floating_sex"
-                disabled={formDisabled}
-                className="outline-none w-full py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300"
-              >
-                <option value="Erkak">Erkak</option>
-                <option value="Ayol">Ayol</option>
-              </select>
             </div>
           </div>
 
